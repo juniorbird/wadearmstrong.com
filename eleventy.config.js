6374d63333928b35db9ff462cbf625ce9af50bf4
Wade Armstrong <wade@wadearmstrong.com>
2023-08-14 15:18:53 -0700

    Add CNAME passthrough copy

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,6 +14,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css",
+		CNAME: "/",
 	});
 
 	// Run Eleventy when these files change:
